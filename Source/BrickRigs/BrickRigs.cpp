f3bb344fee8ae478089df5fd2440d1231980175d
REDACTED_xd <pozetivnyjkriper@gmail.com>
2022-04-05 13:05:40 +0500

    🔹

--- Source/BrickRigs/BrickRigs.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "BrickRigs.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BrickRigs, "BrickRigs" );
