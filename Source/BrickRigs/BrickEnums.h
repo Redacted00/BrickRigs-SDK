b009518b82bd2e9f4cbf07ac6b09b12c122b4166
REDACTED_xd <pozetivnyjkriper@gmail.com>
2022-05-22 19:20:15 +0500

    ➕Add Brick Enums file for UI

---
+++ Source/BrickRigs/BrickEnums.h
@@ -0,0 +1,79 @@
+﻿#pragma once
+
+UENUM(BlueprintType)
+enum class EBrickUIStyleState : uint8
+{
+	Default,
+	Button,
+	Pressed,
+	Focused,
+	Marquee,
+	Foreground,
+	ForegroundPressed,
+	ForegroundFocused,
+	Custom UMETA(Hidden),
+	Max UMETA(Hidden)
+};
+
+UENUM(BlueprintType)
+enum class EBrickUIBrushStyle : uint8
+{
+	Default,
+	Button,
+	FolderButton,
+	MapButton,
+	HoldKeyBorder,
+	BorderlessButton,
+	Custom,
+	Max UMETA(Hidden)
+};
+
+UENUM(BlueprintType)
+enum class EBrickUIColorStyle : uint8
+{
+	Default,
+	Selected,
+	Active,
+	Positive,
+	Negative,
+	Neutral,
+	Custom,
+	Max UMETA(Hidden)
+};
+
+UENUM(BlueprintType)
+enum class EBrickUITextStyle : uint8
+{
+	Default,
+	Bold,
+	Small,
+	Large,
+	Positive,
+	Negative,
+	Neutral,
+	HUD,
+	Logo,
+	Custom,
+	Max UMETA(Hidden)
+};
+
+UENUM(BlueprintType)
+enum class EBrickUIIconAtlas : uint8
+{
+	Default,
+	MenuButtons,
+	MouseKeys,
+	GamepadKeys,
+	HUDIcons,
+	FireModes,
+	AmmoTypes,
+	HealthIcons,
+	Notifications,
+	ConnectionIcons,
+	ServerIcons,
+	HUDNotifications,
+	DashboardIcons,
+	SocialMediaIcons,
+	Custom,
+	Max UMETA(Hidden)
+};
