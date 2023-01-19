ad62f8f79c0ff7f893609143032dc00149aa50f0
REDACTED_xd <38829120+Redacted00@users.noreply.github.com>
2023-01-19 12:37:59 +0500

    Fix weather condition for 1.2+
    Custom Weather don't work correctly for 1.2+

--- Source/BrickRigs/WeatherCondition.h
+++ Source/BrickRigs/WeatherCondition.h
@@ -23,50 +23,61 @@ enum class EPrecipitationType : uint8
     Max UMETA(Hidden)
 };
 
-UCLASS()
-class BRICKRIGS_API UWeatherCondition : public UObject
+
+USTRUCT(BlueprintType)
+struct FDisplayInfo
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
+
+        UPROPERTY(EditAnywhere, BlueprintReadWrite)
+        FText Name;
+};
+
+USTRUCT(BlueprintType)
+struct FWeatherConditionParams
+{
+    GENERATED_BODY()
+
         UPROPERTY(EditAnywhere)
-        FText DisplayName;
-    UPROPERTY(EditAnywhere)
         TSoftObjectPtr<USoundBase> AmbientSound;
     UPROPERTY(EditAnywhere)
-        float Temperature;
+        float Temperature = 20.f;
     UPROPERTY(EditAnywhere)
         float WindSpeed;
-    // The maximum relative variance in speed
     UPROPERTY(EditAnywhere, Meta = (ClampMin = 0.f))
-        float WindSpeedVariance;
-    // Direction the wind is blowing in in degrees from north
-    UPROPERTY(EditAnywhere, Meta = (ClampMin = 0.f, ClampMax = 360.f))
-        float WindDirection;
-    // The maximum deviation from the wind direction
+        float WindSpeedVariance = 0.75f;
     UPROPERTY(EditAnywhere, Meta = (ClampMin = 0.f, ClampMax = 180.f))
-        float WindDirectionVariance;
-    // How often the wind direction and speed is changed, 1 means every second
+        float WindDirectionVariance = 20.f;
     UPROPERTY(EditAnywhere, Meta = (ClampMin = 0.f))
-        float WindPeriod;
+        float WindPeriod = 5.f;
     UPROPERTY(EditAnywhere, Meta = (ClampMin = 0.f, ClampMax = 1.f))
         float CloudDensity;
-    // Height of the clouds above the ground
     UPROPERTY(EditAnywhere)
-        float CloudHeight;
+        float CloudHeight = 100000.f;
     UPROPERTY(EditAnywhere, Meta = (ClampMin = 0.f, ClampMax = 1.f))
         float MaxFogOpacity;
     UPROPERTY(EditAnywhere)
         EPrecipitationType PrecipitationType;
-    // How intensely it is ranining, snowing etc.
     UPROPERTY(EditAnywhere, Meta = (ClampMin = 0.f, ClampMax = 1.f))
-        float PrecipitationIntensity;
-    // How much of the ground is covered in snow
+        float PrecipitationIntensity = 1.f;
     UPROPERTY(EditAnywhere, Meta = (ClampMin = 0.f, ClampMax = 1.f))
         float SnowCoverage;
-    // How much of the ground is covered in rain
     UPROPERTY(EditAnywhere, Meta = (ClampMin = 0.f, ClampMax = 1.f))
         float RainCoverage;
     UPROPERTY(EditAnywhere, Meta = (ClampMin = 0.f))
         float LightningFrequency;
     UPROPERTY(EditAnywhere, Meta = (ClampMin = 0.f))
-        float MinLightningDelay;
+        float MinLightningDelay = 1.5f;
+};
+
+
+UCLASS()
+class BRICKRIGS_API UWeatherCondition : public UObject
+{
+    GENERATED_BODY()
+
+        UPROPERTY(EditAnywhere)
+        FDisplayInfo DisplayInfo;
+    UPROPERTY(EditAnywhere)
+        FWeatherConditionParams Weather;
 };
